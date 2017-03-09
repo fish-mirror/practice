e3f2109a25cb08c27ed65e9711f3c588e8bce097
fish-mirror <yujing02057@gmail.com>
2017-03-10 01:48:13 +0800

    refactor: 重构用户数据操作模块

--- src/com/zjicm/auth/dao/UserDao.java
+++ src/com/zjicm/auth/dao/UserDao.java
@@ -1,14 +1,7 @@
 package com.zjicm.auth.dao;
 
-import com.zjicm.entity.User;
+import com.zjicm.auth.domain.User;
+import com.zjicm.common.lang.sql.dao.BaseDAO;
 
-public interface UserDao {
-	//新建一个用户信息
-	public void save(User u);
-	//通过用户名和密码进行查找,返回用户 
-	public User search(User u);
-	//更新用户密码
-	public boolean updatePwd(String userId, String pwd);
-	//通过id得到user信息
-	public User getById(String userId);
+public interface UserDao extends BaseDAO<User, Integer>{
 }
