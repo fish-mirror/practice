34e37c3f8372dd2dc905fe1e8f9f1065cc959477
fish-mirror <yujing02057@gmail.com>
2017-03-11 19:18:59 +0800

    refactor: 重构企业数据操作模块

--- src/com/zjicm/company/dao/CompanyDao.java
+++ src/com/zjicm/company/dao/CompanyDao.java
@@ -1,22 +1,7 @@
 package com.zjicm.company.dao;
 
-import java.util.List;
+import com.zjicm.common.lang.sql.dao.BaseDAO;
+import com.zjicm.company.domain.Company;
 
-import com.zjicm.entity.Company;
-import com.zjicm.entity.User;
-
-public interface CompanyDao {
-	public void save(User u, Company com);
-	public Company get(String id);
-	public void update(Company com);
-	public void updateImgUrl(String id,String url);
-	
-	public List<Company> find();
-	public List<Company> find(String col);
-	public List<Company> find(int offset,int length);
-	public List<Company> find(String col, int offset,int length);
-	
-	public int count();
-	public int count(String col);
+public interface CompanyDao extends BaseDAO<Company, Integer> {
 }
-	
--- src/com/zjicm/company/service/CompanyService.java
+++ src/com/zjicm/company/service/CompanyService.java
@@ -6,130 +6,183 @@ import javax.annotation.Resource;
 
 import com.zjicm.company.dao.CompanyDao;
 import com.zjicm.dto.Page;
-import com.zjicm.entity.Company;
+import com.zjicm.company.domain.Company;
 import com.zjicm.entity.Cooperate;
-import com.zjicm.entity.User;
+import com.zjicm.auth.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.zjicm.cooperation.dao.CooperateDao;
 
 @Component
-public class CompanyService implements ICompanyService{
+public class CompanyService implements ICompanyService {
+    @Autowired
+    private CompanyDao companyDao;
+    @Autowired
+    private CooperateDao cooperateDao;
 
-	CompanyDao companyDao;
-	CooperateDao cooperateDao;
+    @Override
+    public void save(User u, Company com) {
 
-	public CompanyDao getCompanyDao() {
-		return companyDao;
-	}
-	@Resource
-	public void setCompanyDao(CompanyDao companyDao) {
-		this.companyDao = companyDao;
-	}
-	
-	
-	public CooperateDao getCooperateDao() {
-		return cooperateDao;
-	}
-	@Resource
-	public void setCooperateDao(CooperateDao cooperateDao) {
-		this.cooperateDao = cooperateDao;
-	}
-	
-	@Override
-	public List<Company> getCompanyList() {
-		
-		return companyDao.find();
-	}
-	@Override
-	public List<Company> getCompanyList(String colId) {
+    }
 
-		return companyDao.find(colId);
-	}
-	@Override
-	public Page pageForCompany(int pageSize, int page) {
-		int count = companyDao.count();
-		count = companyDao.count();
-		int totalPage = Page.countTotalPage(pageSize, count);
-		int offset = Page.countOffset(pageSize, page);
-		int length = pageSize;
-		int currentPage = Page.countCurrentPage(page);
-		
-		List<Company> list = companyDao.find(offset, length);
-		
-		Page p = new Page();
-		p.setPageSize(pageSize);
-		p.setCurrentPage(currentPage);
-		p.setAllRow(count);
-		p.setTotalPage(totalPage);
-		p.setList(list);
-		p.init();
-		
-		return p;
-	}
-	@Override
-	public Page pageForCompany(String colId, int pageSize,int page){
-		int count = companyDao.count(colId);
-		count = companyDao.count();
-		int totalPage = Page.countTotalPage(pageSize, count);
-		int offset = Page.countOffset(pageSize, page);
-		int length = pageSize;
-		int currentPage = Page.countCurrentPage(page);
-		
-		List<Company> list = companyDao.find(offset, length);
-		
-		Page p = new Page();
-		p.setPageSize(pageSize);
-		p.setCurrentPage(currentPage);
-		p.setAllRow(count);
-		p.setTotalPage(totalPage);
-		p.setList(list);
-		p.init();
-		
-		return p;
-	}
+    @Override
+    public void saveAndCoop(User u, Company com, String colId) {
 
-	@Override
-	public boolean updateComImg(String id, String url) {
-		try{
-			companyDao.updateImgUrl(id, url);
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	@Override
-	public void updateCompany(Company Col) {
-		companyDao.update(Col);
-	}
-	
-	@Override
-	public Company getCompany(String id) {
-		return companyDao.get(id);
-	}
-	@Override
-	public void save(User u, Company com) {
-		companyDao.save(u, com);
-	}
-	@Override
-	public void saveAndCoop(User u, Company com, String colId) {
-		this.save(u, com);
-		cooperateDao.save(new Cooperate(colId, com.getId()));
-	}
-	@Override
-	public void deleteCoop(Integer coopId) {
-		cooperateDao.delete(new Cooperate(coopId));
-	}
-	@Override
-	public void deleteCoop(String colId,String comId) {
-		cooperateDao.delete(colId,comId);
-	}
-	@Override
-	public void addCoop(String colId, String comId) {
-		cooperateDao.save(new Cooperate(colId,comId));
-		
-	}
-	
-	
+    }
+
+    @Override
+    public List<Company> getCompanyList() {
+        return null;
+    }
+
+    @Override
+    public List<Company> getCompanyList(String colId) {
+        return null;
+    }
+
+    @Override
+    public Page pageForCompany(int pageSize, int page) {
+        return null;
+    }
+
+    @Override
+    public Page pageForCompany(String colId, int pageSize, int page) {
+        return null;
+    }
+
+    @Override
+    public boolean updateComImg(String id, String url) {
+        return false;
+    }
+
+    @Override
+    public void updateCompany(Company com) {
+
+    }
+
+    @Override
+    public Company getCompany(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteCoop(Integer coopId) {
+
+    }
+
+    @Override
+    public void deleteCoop(String colId, String comId) {
+
+    }
+
+    @Override
+    public void addCoop(String colId, String comId) {
+
+    }
+
+
+//    @Override
+//    public List<Company> getCompanyList() {
+//
+//        return companyDao.find();
+//    }
+//
+//    @Override
+//    public List<Company> getCompanyList(String colId) {
+//
+//        return companyDao.find(colId);
+//    }
+//
+//    @Override
+//    public Page pageForCompany(int pageSize, int page) {
+//        int count = companyDao.count();
+//        count = companyDao.count();
+//        int totalPage = Page.countTotalPage(pageSize, count);
+//        int offset = Page.countOffset(pageSize, page);
+//        int length = pageSize;
+//        int currentPage = Page.countCurrentPage(page);
+//
+//        List<Company> list = companyDao.find(offset, length);
+//
+//        Page p = new Page();
+//        p.setPageSize(pageSize);
+//        p.setCurrentPage(currentPage);
+//        p.setAllRow(count);
+//        p.setTotalPage(totalPage);
+//        p.setList(list);
+//        p.init();
+//
+//        return p;
+//    }
+//
+//    @Override
+//    public Page pageForCompany(String colId, int pageSize, int page) {
+//        int count = companyDao.count(colId);
+//        count = companyDao.count();
+//        int totalPage = Page.countTotalPage(pageSize, count);
+//        int offset = Page.countOffset(pageSize, page);
+//        int length = pageSize;
+//        int currentPage = Page.countCurrentPage(page);
+//
+//        List<Company> list = companyDao.find(offset, length);
+//
+//        Page p = new Page();
+//        p.setPageSize(pageSize);
+//        p.setCurrentPage(currentPage);
+//        p.setAllRow(count);
+//        p.setTotalPage(totalPage);
+//        p.setList(list);
+//        p.init();
+//
+//        return p;
+//    }
+//
+//    @Override
+//    public boolean updateComImg(String id, String url) {
+//        try {
+//            companyDao.updateImgUrl(id, url);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//        return true;
+//    }
+//
+//    @Override
+//    public void updateCompany(Company Col) {
+//        companyDao.update(Col);
+//    }
+//
+//    @Override
+//    public Company getCompany(String id) {
+//        return companyDao.get(id);
+//    }
+//
+//    @Override
+//    public void save(User u, Company com) {
+//        companyDao.save(u, com);
+//    }
+//
+//    @Override
+//    public void saveAndCoop(User u, Company com, String colId) {
+//        this.save(u, com);
+//        cooperateDao.save(new Cooperate(colId, com.getId()));
+//    }
+//
+//    @Override
+//    public void deleteCoop(Integer coopId) {
+//        cooperateDao.delete(new Cooperate(coopId));
+//    }
+//
+//    @Override
+//    public void deleteCoop(String colId, String comId) {
+//        cooperateDao.delete(colId, comId);
+//    }
+//
+//    @Override
+//    public void addCoop(String colId, String comId) {
+//        cooperateDao.save(new Cooperate(colId, comId));
+//
+//    }
 }
