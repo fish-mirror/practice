e094a5d968a74daabdcb7ebdf303ab91ac7f7cfb
yujing <yujing@dxyer.com>
2017-04-11 23:27:28 +0800

    change: 修改用户数据访问方法和相关引用，编写测试类

--- src/com/zjicm/auth/dao/UserDaoImpl.java
+++ src/com/zjicm/auth/dao/UserDaoImpl.java
@@ -3,10 +3,28 @@ package com.zjicm.auth.dao;
 
 import com.zjicm.auth.domain.User;
 import com.zjicm.common.dao.PracticeBaseDaoImpl;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class UserDaoImpl extends PracticeBaseDaoImpl<User, Integer> implements UserDao {
 
 
+    @Override
+    public User getByNumPwd(String account, String password) {
+        List<Criterion> criterions = new ArrayList<>(2);
+        criterions.add(Restrictions.eq("number", account));
+        criterions.add(Restrictions.eq("password", password));
+        return this.get(criterions, null);
+    }
+
+
+    @Override
+    public User getByNum(String number) {
+        return this.getByField("number", number);
+    }
 }
