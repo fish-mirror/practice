93e34eef1457757fdbbf4861a0f76e79f1b77851
fish-mirror <yujing02057@gmail.com>
2017-04-11 21:21:03 +0800

    add: 测试模块

---
+++ test/com/zjicm/DaoTest.java
@@ -0,0 +1,27 @@
+package com.zjicm;
+
+import org.junit.Test;
+
+/**
+ * Created by yujing on 2017/4/11.
+ */
+public class DaoTest extends BaseTest {
+    @Test
+    public void saveTest(){
+
+    }
+
+    @Test
+    public void getTest(){
+
+    }
+    @Test
+    public void listTest(){
+
+    }
+
+    @Test
+    public void deleteTest(){
+
+    }
+}
--- test/com/zjicm/dao/StudentDaoTest.java
+++ test/com/zjicm/dao/StudentDaoTest.java
@@ -1,31 +1,31 @@
 package com.zjicm.dao;
 
-import java.util.List;
-
+import com.zjicm.DaoTest;
 import com.zjicm.student.dao.StudentDao;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
-@ContextConfiguration("classpath:beans.xml")
-public class StudentDaoTest extends   
-	AbstractTransactionalJUnit4SpringContextTests {
-	
-	@Autowired  
-	private StudentDao studentDao;
-	
-	@Test  
-	public void countGraduateStatus(){  
+public class StudentDaoTest extends DaoTest{
 
-		List<Object[]> list = studentDao.countGraduateStatus((short)1);
-		for(Object[] link : list){  
-			  
-		    short status = (short) link[0];  
-		  
-		    int s = ((Long) link[1]).intValue();  
-		  
-		    System.out.println("==========");
-		}  
-	}  
+    @Autowired
+    private StudentDao studentDao;
+
+    @Override
+    public void saveTest() {
+        super.saveTest();
+    }
+
+    @Override
+    public void getTest() {
+        super.getTest();
+    }
+
+    @Override
+    public void listTest() {
+        super.listTest();
+    }
+
+    @Override
+    public void deleteTest() {
+        super.deleteTest();
+    }
 }
